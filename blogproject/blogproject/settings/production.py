87930b607222fb8cda1a568b8934cc3d74d725ab
honychen <514292146@qq.com>
2019-11-11 01:09:05 -0500

    separate settings.py

---
+++ blogproject/blogproject/settings/production.py
@@ -0,0 +1,7 @@
+from .common import *
+ 
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+DEBUG = False
+#ALLOWED_HOSTS = ['hellodjango-blog-tutorial-demo.zmrenwu.com']
+
+ALLOWED_HOSTS = ['192.168.1.164','127.0.0.1','localhost','.honychen.com']
