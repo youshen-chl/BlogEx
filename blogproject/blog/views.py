a73a16f8719c0d46ba33230cdf224d77b2a452f2
youshen-chl <514292146@qq.com>
2019-11-12 13:28:41 +0800

    实现分页功能

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -7,15 +7,32 @@ from django.utils.text import slugify
 import markdown
 from markdown.extensions.toc import TocExtension
 import re
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def index(request):
 
     post_list = Post.objects.all()
 #    return HttpResponse('欢迎大家来到我的博客！')
+    paginator = Paginator(post_list, 2)
+
+    page = request.GET.get('page')
+    try:
+        contacts = paginator.page(page)
+    except PageNotAnInteger:
+        # 如果用户请求的页码号不是整数，显示第一页
+        contacts = paginator.page(1)
+    except EmptyPage:
+        # 如果用户请求的页码号超过了最大页码号，显示最后一页
+        contacts = paginator.page(paginator.num_pages)
+
+    is_paginated = True if paginator.num_pages >= 2 else False
+
     return render(request, 'blog/index.html',context={
         # 'title' : '我的博客首页',
         # 'welcome' : '欢迎访问我的博客',
-        'post_list' : post_list
+        'page_obj'  : contacts ,
+        'is_paginated' : is_paginated ,
+        'paginator' : paginator,
     })
 
 def category(request, pk):
@@ -60,3 +77,13 @@ def detail(request, pk):
     #                                     'markdown.extensions.toc',
     #                                 ]) 
     return render(request,'blog/detail.html',context={'post' : post})
+
+
+# from django.views.generic.list import ListView
+
+# class IndexView(ListView):
+#     model = Post
+#     template_name = 'blog/index.html'
+#     context_object_name = 'post_list'
+#     # 指定 paginate_by 属性后开启分页功能，其值代表每一页包含多少篇文章
+#     paginate_by = 2
--- blogproject/blogproject/settings/local.py
+++ blogproject/blogproject/settings/local.py
@@ -1,5 +1,6 @@
 from .common import *
  
-SECRET_KEY = 'development-secret-key'
+# SECRET_KEY = '()!r2wg4+4=mnab@!zbi-*hr%b^qk3q34r3z_o^glopyj%*i3('
+SECRET_KEY =  'development-secret-key'
 DEBUG = True
 ALLOWED_HOSTS = ['*']
